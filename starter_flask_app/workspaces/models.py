93666dfaa31872109609a0f6a1608fd7a7b6bf3e
To4ka46 <samp.tawer228@bk.ru>
2026-08-02 22:12:46 +0300

    Delete starter_flask_app directory
    DEL

--- starter_flask_app/workspaces/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Workspace(models.Model):
-    name = models.CharField(max_length=100, verbose_name='Название рабочего места')
-    location = models.CharField(max_length=200, verbose_name='Расположение')
-    description = models.TextField(blank=True, verbose_name='Описание')
-
-    def __str__(self):
-        return self.name
