bcc8a3e2fc4a5a7ea0722d5e49acdf16153b970d
To4ka46 <samp.tawer228@bk.ru>
2026-08-17 22:10:00 +0300

    Delete starter_flask_app directory
    del

--- starter_flask_app/workspaces/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse("Привет! Это главная страница.")
-    # или render(request, 'home.html') если используешь шаблоны
